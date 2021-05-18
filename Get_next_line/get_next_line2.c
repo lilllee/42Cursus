a5fe2c9d9f82f122decca1f6b2377372c7b6b404
Teasu Kim <tekim@c7r7s8.42seoul.kr>
2021-05-18 18:40:54 +0900

    GNL

--- Get_next_line/get_next_line2.c
+++ Get_next_line/get_next_line2.c
@@ -10,6 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void swap(int a, int b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int get_next_line(int fd, char *line)
+{
+	line = malloc(sizeof(char) * line_size);
+}
+
 int get_next_line(int fd, char **line)
 {
+	*line = malloc(sizeof(char) * line_size);
+	static int a = 0;
+}
+
+int main()
+{
+	char *new_line;
+	get_next_line(fd, &new_line);
 }
